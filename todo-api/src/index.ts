72f6cb2b6dc1cfefea77208b3274f3864b29ce8e
hello-yuki0409 <bobu2784@gmail.com>
2025-11-12 13:36:03 +0900

    chore: Hono導入

---
+++ todo-api/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get("/todos", (c) => {
+  return c.json({ todos });
+});
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
