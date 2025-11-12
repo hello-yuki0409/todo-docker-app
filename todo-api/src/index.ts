55fc0ee1c696fc219d9c1b97d6af77b7c008d2e4
hello-yuki0409 <bobu2784@gmail.com>
2025-11-12 13:47:19 +0900

    refactor: エンドポイント追加

--- todo-api/src/index.ts
+++ todo-api/src/index.ts
@@ -19,6 +19,28 @@ app.get("/todos", (c) => {
   return c.json({ todos });
 });
 
+app.post("/todos", async (c) => {
+  const { title } = await c.req.json();
+  const todo: Todo = {
+    id: todos.length + 1,
+    title,
+    completed: false,
+  };
+  todos.push(todo);
+  return c.json({ todo });
+});
+
+app.put("/todos/:id", async (c) => {
+  const { id } = c.req.param();
+  const { completed } = await c.req.json();
+  const todo = todos.find((todo) => todo.id === Number(id));
+  if (!todo) {
+    return c.notFound();
+  }
+  todo.completed = completed;
+  return c.json({ todo });
+});
+
 serve({
   fetch: app.fetch,
   port: 3000
