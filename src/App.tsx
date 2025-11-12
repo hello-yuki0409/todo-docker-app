9f49bb788a90bb23dbd5051b133bf5dc91929d2a
hello-yuki0409 <bobu2784@gmail.com>
2025-11-12 11:25:37 +0900

    ファーストコミット

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Todo 1",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      completed: false,
+    },
+  ]);
+
+  const handleAddTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        title: title,
+        completed: false,
+      },
+    ]);
+    setTitle("");
+  };
+
+  return (
+    <>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>{todo.title}</div>
+        ))}
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="新しいTodo"
+        />
+        <button onClick={handleAddTodo}>追加</button>
+        <div>{title}</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
